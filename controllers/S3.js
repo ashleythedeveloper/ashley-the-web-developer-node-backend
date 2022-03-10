1bfc5b817c3e4d7afd9c674296ce6b919f36c608
ashleythewebdeveloper <ashley@harwoodslawncare.com.au>
2022-03-11 09:51:38 +1100

    Added new AWS S3 endpoint.

--- controllers/S3.js
+++ controllers/S3.js
@@ -1,20 +1,25 @@
 const AWS = require('aws-sdk');
 
+var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
+
+AWS.config.credentials = credentials;
+AWS.config.update({ region:'ap-southeast-2' });
+
 // Create S3 service object
-s3 = new AWS.S3({ endpoint: 'https://sgp1.digitaloceanspaces.com', region: "sgp1", http_continue_timeout: 0 });
+s3 = new AWS.S3({ endpoint: 'https://s3.ap-southeast-2.amazonaws.com', region: "ap-southeast-2", http_continue_timeout: 0 });
 
 const bucketName = process.env.AWS_BUCKET_NAME
 
 // Produce a URL for given object
 exports.GetObjectUrl = async (imageName) => {
-  const url = await s3.getSignedUrl('getObject', { Bucket: bucketName, Key: `images/${imageName}` })
+  const url = await s3.getSignedUrl('getObject', { Bucket: bucketName, Key: `${imageName}` })
   return url
 };
 
 
 // Create an image object
 exports.CreateObject = (name, file, fileType) => {
-  s3.upload({ Bucket: bucketName, Key: `images/${name}${fileType}`, Body: file }, (err, data) => {
+  s3.upload({ Bucket: bucketName, Key: `${name}${fileType}`, Body: file }, (err, data) => {
     if (!err) {
       console.log(data)
     } else {
@@ -29,7 +34,7 @@ exports.CreateObject = (name, file, fileType) => {
 
 exports.DeleteObject = async (fileName) => {
   console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Removing Image!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-  await s3.deleteObject({ Bucket: bucketName, Key: `images/${fileName}` }, (err, res) => {
+  await s3.deleteObject({ Bucket: bucketName, Key: `${fileName}` }, (err, res) => {
     if (!err) {
       console.log("No Error")
       console.log(res);

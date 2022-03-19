88136dcf85dbfd467898b7c824b4dff4bad053e9
ashleythewebdeveloper <ashley@harwoodslawncare.com.au>
2022-03-20 07:03:56 +1100

    Removed console.log.

--- controllers/metaData.js
+++ controllers/metaData.js
@@ -2,7 +2,6 @@ const DB = require('./database');
 
 
 exports.GetMetaData = async (req, res, next) => {
-  console.log(req.body.location)
   const metaData = await DB.getMetaData(req.body.location);
 
   res.status(201).send(metaData);
